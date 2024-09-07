6586d5efe77ac5553dd10e76f9d4cb379a683d54
Jonas Voisard <jvoisard@c1r11s8.42lausanne.ch>
2024-09-07 16:49:59 +0200

    set header

--- ex00/print_number.c
+++ ex00/print_number.c
@@ -1,5 +1,10 @@
+#include "print_number.h"
 
 void print_number(char *number, char *dict) {
 
 	// PRINT
+}
+
+t_label	*read_dict(char *dict) {
+
 }
--- ex00/print_number.h
+++ ex00/print_number.h
@@ -1,26 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   print_number.h                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: jvoisard <jvoisard@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/09/07 15:59:30 by jvoisard          #+#    #+#             */
-/*   Updated: 2024/09/07 16:47:45 by jvoisard         ###   ########.fr       */
+/*   Created: 2024/09/07 16:43:12 by jvoisard          #+#    #+#             */
+/*   Updated: 2024/09/07 16:46:47 by jvoisard         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "print_number.h"
+#ifndef PRINT_NUMBER_H
+# define PRINT_NUMBER_H
 
-int	main(int ac, char **av)
+typedef struct s_label	t_label;
+struct	s_label
 {
-	if (ac != 2 && ac != 3)
-		return (1);
-	if (ac == 2)
-	{
-		print_number(av[1], "numbers.dict");
-		return (0);
-	}
-	print_number(av[1], av[2]);
-	return (0);
-}
+	char	*key;
+	char	*label;
+};
+
+#endif
