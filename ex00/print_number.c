f9fcf408e2e217eda7da36544876e2cf1b021375
Alexis Favre <alfavre@c1r9s8.42lausanne.ch>
2024-09-07 16:51:23 +0200

    add some code to open, read and close file

--- ex00/print_number.c
+++ ex00/print_number.c
@@ -6,5 +6,42 @@ void print_number(char *number, char *dict) {
 }
 
 t_label	*read_dict(char *dict) {
+	char	*buff;
+	int fd;
+	char c;
+	int nb_read;
+	int count;
+	int	index;
 
+	index = 0;
+	fd = open(NAME_FILE, O_RDONLY);
+	if (fd == -1)
+	{
+		write(1, "Not able to open the file.", sizeof("Not able to open the file."));
+		return (0);
+	}	
+	nb_read = 1;
+	count = 0;
+	c = '\0';
+	while (nb_read != 0)
+	{
+		nb_read = read(fd, &c, 1);
+		count++;
+	}
+	close(fd);
+	fd = open(NAME_FILE, O_RDONLY);
+	buff = malloc(sizeof(char) * count);
+	while (nb_read != 0)
+	{
+		nb_read = read(fd, buff, count);
+		if (nb_read == -1)
+		{
+			write(1, "Read error!\n", sizeof("Read error\n"));
+			return (1);
+		}
+		buff[count] = '\0';
+	}
+	write(1, buff, count);
+	close(fd);
+	return (0);
 }
